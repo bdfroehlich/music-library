4dde5b5f38e3c3caf19b9f10bbe92a877dec6fc8
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-30 18:42:18 -0500

    adding server/index.js and associated dependencies

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -7,12 +7,15 @@ function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
-    return (
-        <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
-        </div>
-    )
+    useEffect(() => {
+        const API_URL = 'http://localhost:4000/album/${id}'
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
 }
 
 export default ArtistView
