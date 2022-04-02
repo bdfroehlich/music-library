c4fb90cfc5ce0a2d4e7de0da0efc533fab98a416
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-04-01 22:54:13 -0500

    adding helper.js

---
+++ src/helper.js
@@ -0,0 +1,38 @@
+const API_URL = 'https://itunes.apple.com/search?term='
+
+const fetchSearch = async (search) => {
+    const response = await fetch(API_URL + search)
+    const resData = await response.json()
+    return resData.results
+}
+
+//wrapPromise takes the promise from fetchSearch as an argument
+const wrapPromise = (promise) => {
+    // assumed default state of promise is pendingg
+    let status = 'pending'
+    // result stores data from promise
+    let result = ''
+    // resolved promise should flag status to success catch to error
+    let suspender = promise.then(response => {
+        status = 'success'
+        result = response
+    }, err => {
+        status = 'error'
+        result = err
+    })
+
+    // returned object depending on status
+    return {
+        read() {
+            // if the promise hasn't triggered
+            if(status === 'pending') {
+                throw suspender
+            }
+            else if (status === 'error') {
+                throw result
+            }
+            // if status is neither pending nor error send result forward
+            return result
+        }
+    }
+}
