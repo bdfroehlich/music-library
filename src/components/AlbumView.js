91760feed7d17145f04d214099215a3b1b630aea
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-04-01 20:40:55 -0500

    adding artistview and album view for react-router-dom

---
+++ src/components/AlbumView.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default Albumview = () => {
+    const [ albumData, setAlbumData ] = useState([]);
+
+    return (
+        <div>
+            <p>
+                Album data goes here!
+            </p>
+        </div>
+    )
+}
---
+++ src/components/ArtistView.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default Artistview = () => {
+    const [ artistData, setArtistData ] = useState([]);
+
+    return (
+        <div>
+            <p>
+                This is where the artist data goes!
+            </p>
+        </div>
+    )
+}
